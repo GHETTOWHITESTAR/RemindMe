a4b3a7d5166e075b92f22738a4f0338c001c59a1
GHETTOWHITESTAR <mudachi30@gmail.com>
2018-07-08 23:51:04 +0400

    Toolbar

--- app/src/main/java/com/example/user/remindme/MainActivity.java
+++ app/src/main/java/com/example/user/remindme/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.user.remindme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
